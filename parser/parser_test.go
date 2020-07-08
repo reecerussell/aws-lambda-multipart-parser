39e2bfa2c31b2b1bb43eb65fb38dbf8f6d2712b4
Reece Russell <reece@prominentmedia.com>
2020-07-08 09:17:22 +0100

    added more tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"encoding/base64"
 	"github.com/aws/aws-lambda-go/events"
 	"testing"
 )
@@ -20,29 +21,122 @@ Content-Disposition: form-data; name="text"
 
 text default
 -----------------------------9051914041544843365972754266
-Content-Disposition: form-data; name="file1"; filename="a.txt"
+Content-Disposition: form-data; name="file"; filename="file.txt"
 Content-Type: text/plain
 
-Content of a.txt.
+Hello World :)
 
 -----------------------------9051914041544843365972754266`
+const testContentType = "multipart/form-data; boundary=---------------------------9051914041544843365972754266"
 
 func TestParse(t *testing.T) {
 	e := events.APIGatewayProxyRequest{
 		Body: testBody,
 		Headers: map[string]string{
-			"Content-Type": "multipart/form-data; boundary=---------------------------9051914041544843365972754266",
+			"Content-Type": testContentType,
 		},
 	}
 
-	data := Parse(e)
+	data, err := Parse(e)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+		return
+	}
 
 	txt, ok := data.Get("text")
 	if !ok {
-		t.Errorf("Expected to have a field for with name 'text'.\n")
+		t.Errorf("Expected to have a field for name 'text'.")
 	} else {
 		if txt != "text default" {
 			t.Errorf("Expected to have 'text default' in field 'text', but got: %s.\n", txt)
 		}
 	}
+
+	file, ok := data.File("file")
+	if !ok {
+		t.Errorf("Expected to have a file for name 'file'.")
+	} else {
+		if file.Type != "file" {
+			t.Errorf("Expected file type to be 'file', but got: %s", file.Type)
+		}
+
+		if file.Filename != "file.txt" {
+			t.Errorf("Expected filename to be 'file.txt', but got: %s", file.Filename)
+		}
+
+		if file.ContentType != "text/plain" {
+			t.Errorf("Expected content type to be 'text/plain' but got: %s", file.ContentType)
+		}
+
+		if string(file.Content) != "Hello World :)" {
+			t.Errorf("Exepected content to be 'Hello World :)' but got: %s", string(file.Content))
+		}
+	}
+}
+
+func TestParseWithBase64(t *testing.T) {
+	data := base64.StdEncoding.EncodeToString([]byte(testBody))
+	e := events.APIGatewayProxyRequest{
+		Body: data,
+		IsBase64Encoded: true,
+		Headers: map[string]string{
+			"Content-Type": testContentType,
+		},
+	}
+
+	_, err := Parse(e)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	t.Run("Invalid Base64 Data", func(t *testing.T) {
+		e := events.APIGatewayProxyRequest{
+			Body: "this isn't base64 data ;)",
+			IsBase64Encoded: true,
+			Headers: map[string]string{
+				"Content-Type": testContentType,
+			},
+		}
+
+		_, err := Parse(e)
+		if err == nil {
+			t.Errorf("expected an error")
+		}
+	})
+}
+
+func TestGetBoundary(t *testing.T) {
+	e := events.APIGatewayProxyRequest{
+		Headers: map[string]string{
+			"Content-Type": testContentType,
+		},
+		Body: testBody,
+	}
+
+	_, err := Parse(e)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	t.Run("Missing Content-Type Header", func(t *testing.T) {
+		e := events.APIGatewayProxyRequest{}
+
+		_, err := Parse(e)
+		if err == nil {
+			t.Errorf("expected an error")
+		}
+	})
+
+	t.Run("Invalid Content-Type Header", func(t *testing.T) {
+		e := events.APIGatewayProxyRequest{
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+		}
+
+		_, err := Parse(e)
+		if err == nil {
+			t.Errorf("expected an error")
+		}
+	})
 }
